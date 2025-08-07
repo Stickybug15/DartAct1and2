0c990c3facfffc191ec17ce9292cc13569efebe6
Ryan Pantua IT303 <pantuaryan15@gmail.com>
2025-08-07 17:22:54 +0800

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,11 @@
+// void main(){
+//   String greeting = 'Welcome,';
+//   String name = 'Alice!';
+//   print('$greeting $name');
+// }
+void main(){
+  String greeting = 'Welcome,';
+  dynamic name = 25;
+  print('$greeting $name!');
+}
+//the usage of dynamic in dart is to allow any data type to be assigned on it.
